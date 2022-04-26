85ee9822cf006e6d7ad73bc6076aab50dabac8ac
JariDeGraeve <degraevjari@live.be>
2022-04-26 19:09:41 +0200

    fixed measurements/model + added sphere to visualize environment better

--- test.m
+++ test.m
@@ -32,7 +32,60 @@
 % integral2(q_gas , 0, 2*pi, 0, pi )
 
 
-theta = linspace(0, 2*pi);
-polarplot(theta, 1 - 0.8 *cos(theta + pi), "LineWidth", 3)
-hold on;
-polarplot(theta,0.02+zeros(size(theta)), "LineWidth", 6)
+% theta = linspace(0, 2*pi);
+% polarplot(theta, 1 - 0.8 *cos(theta + pi), "LineWidth", 3)
+% hold on;
+% polarplot(theta,0.02+zeros(size(theta)), "LineWidth", 6)
+
+% [X,Y,Z] = meshgrid(-5:0.2:5);
+% V = X.*exp(-X.^2-Y.^2-Z.^2);
+% 
+% [xsurf,ysurf] = meshgrid(-2:0.2:2);
+% zsurf = -xsurf.^2-ysurf.^2;
+% slice(X,Y,Z,V,xsurf,ysurf,zsurf)
+
+% clc,clear
+%     % generate some data
+% theta1 = linspace(-1,1,60)*pi;
+% phi1 = linspace(-1,1,20)*pi/2;
+% r1 = 0:10;
+% [theta, phi, r] = meshgrid(theta1,phi1,r1);
+% f = r + cos(10*theta);
+%     % boundaries of volume
+% [X,Y,Z] = sphere(20);
+% X = r1(end)*X;
+% Y = r1(end)*Y;
+% Z = r1(end)*Z;
+%     % create isosurface where f=5
+% p = patch(isosurface(theta,phi,r,f,5));
+%     % extract spherical data
+% fc = get(p,'Faces');
+% vc = get(p,'Vertices');
+%     % convert spherical data to cartesian
+% [x1,y1,z1] = sph2cart(vc(:,1),vc(:,2),vc(:,3));
+% vc = [x1 y1 z1];
+%     % plot boundaries
+% cla
+% surf(X,Y,Z,'Facecolor','none','edgeColor',[1 1 1]*0.5)
+% hold on
+%     % plot data f=5 in cartesian
+% h = patch('Faces',fc,'Vertices',vc);
+% hold off
+% set(h,'FaceColor','r','EdgeColor','none');
+% camlight 
+% lighting gouraud
+% 
+
+[x,y,z] = meshgrid(-10:0.1:10);
+data = x.^2 + y.^2 + z.^2;
+cdata = smooth3(rand(size(data)),'box',7);
+p = patch(isosurface(x,y,z,data));
+isonormals(x,y,z,data,p)
+isocolors(x,y,z,cdata,p)
+p.FaceColor = 'interp';
+p.EdgeColor = 'none';
+view(150,30)
+daspect([1 1 1])
+axis tight
+camlight
+lighting gouraud
